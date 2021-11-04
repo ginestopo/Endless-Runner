e448c8ba39736d79e2fb247e4b287dc8d61360bd
Ginés Díaz <ginestopo@gmail.com>
2021-11-04 10:56:40 +0100

    Parallax with motion
    parallax with motion, changed moving camera instead of moving blocks

--- scenes/world/platform.gd
+++ scenes/world/platform.gd
@@ -4,8 +4,8 @@ extends StaticBody2D
 export(int, -1000, 0) var speed := 200
 
 
-func _physics_process(delta: float) -> void:
-	position.x -= speed * delta
+#func _physics_process(delta: float) -> void:
+#	position.x -= speed * delta
 
 
 func _on_visibility_notifier_2d_screen_exited() -> void:
--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -6,11 +6,13 @@ var _Platform := preload("res://scenes/world/platform.tscn")
 var _start_time := OS.get_ticks_msec()
 
 var rng = RandomNumberGenerator.new()
+export (int) var velocity 
 
 
 func _process(_delta: float) -> void:
 # warning-ignore:integer_division
 	$UI/Score.text = str((OS.get_ticks_msec() - _start_time) / 1_000)
+	$Camera2D.position.x += velocity*_delta
 
 
 func _on_restart_pressed() -> void:
@@ -31,7 +33,6 @@ func _on_platform_timer_timeout() -> void:
 	rng.randomize()
 	#random x size for each block 
 	p.scale.x = rng.randf_range(0.5,1.0)
-	print(p.scale.x)
-	p.position.x = 720
+	p.position.x = $Camera2D.position.x + 960
 	p.position.y = randi() % 135 + 68
 
