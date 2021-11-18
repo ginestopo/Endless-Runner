f5f9c14643614e1e7e1cc2d272f3d752e40a1515
Ginés Díaz <ginestopo@gmail.com>
2021-11-18 12:57:54 +0100

    Fixed the menu fade in, particles, and shoot mechanic
    Fixed the menu fade in (not working), particles spread radius, and shoot mechanic to an automatic machine gun with cadence parameter

--- scenes/gun/shootGun.gd
+++ scenes/gun/shootGun.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
 export var bulletScene : PackedScene
+export (float) var cadence_set = 1.5 
+var cadence = 0
 
 signal shot
 
-func _input(event):
-	if event.is_action_pressed("shoot"):
+#func _input(event):
+#	if event.is_action_pressed("shoot"):
+#		var bullet = bulletScene.instance() as Node2D
+#		get_tree().get_root().add_child(bullet)
+#		bullet.global_position = self.global_position
+#		bullet.direction = (get_global_mouse_position()-global_position).normalized()
+#		bullet.rotation = bullet.direction.angle()
+#
+#		emit_signal("shot") #used for the camera to know when to shake
+		
+func _process(delta):
+	if Input.is_action_pressed("shoot") and (cadence > cadence_set):
 		var bullet = bulletScene.instance() as Node2D
 		get_tree().get_root().add_child(bullet)
 		bullet.global_position = self.global_position
@@ -13,3 +25,7 @@ func _input(event):
 		bullet.rotation = bullet.direction.angle()
 		
 		emit_signal("shot") #used for the camera to know when to shake
+		cadence = 0;
+	else:
+		cadence += delta*1
+
